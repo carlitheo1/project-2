97978ddab1803ffbc2a86ffc3901ffe2b63f5b54
carlitheo1 <carli.theodore@yahoo.com>
2023-10-03 10:59:52 -0500

    added nav bar
    -added button to the about us page
    -

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
