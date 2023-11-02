f2f503991b599e6ad6c44839e602c4afae7126d6
carlitheo1 <carli.theodore@yahoo.com>
2023-11-02 06:34:35 -0500

    idk

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // JS scripts placed here
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
